981e04fa047dcabb7f4ed96ab2d87308b21b907a
azul-de-prusia <51216447+azul-de-prusia@users.noreply.github.com>
2019-06-02 16:22:28 -0500

    Add files via upload
    imprimir el resultado del codificador

--- src/cipher.js
+++ src/cipher.js
@@ -3,16 +3,15 @@ function cipher(string,incremento){
 
 var longitud = string.length;
 var inicio = 0;
-
   while (inicio  < longitud)
-  {
-     var x = string.charCodeAt(inicio);
-     var formula = (x -65+incremento)%26+65;
-     console.log(String.fromCharCode(formula));
-    inicio++;
+  { 
+     //console.log(inicio);
+     var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
+     var formula = (x -65+incremento)%26+65; //formula laboratoria
+     var letracodificada = String.fromCharCode(formula);
+
+     document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
+     //imprimir(letracodificada);
+     inicio++;
   }
-
-
-
 }
-cipher ("GALIMATAZO", 4);

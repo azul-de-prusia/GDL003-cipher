803bb070baf98057332dacc4f76e888969258f88
laboratoria-192 <mara_lilian@hotmail.com>
2019-06-02 14:30:19 -0500

    funcion cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,18 @@
-window.cipher = {
-  // ... 
-};
+//cipher
+function cipher(string,incremento){
+
+var longitud = string.length;
+var inicio = 0;
+
+  while (inicio  < longitud)
+  {
+     var x = string.charCodeAt(inicio);
+     var formula = (x -65+incremento)%26+65;
+     console.log(String.fromCharCode(formula));
+    inicio++;
+  }
+
+
+
+}
+cipher ("GALIMATAZO", 4);

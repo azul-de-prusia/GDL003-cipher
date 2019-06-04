f5e029dfd04fda9b1f2cf06e62798b8bec76bfb7
azul-de-prusia <51216447+azul-de-prusia@users.noreply.github.com>
2019-06-04 15:43:04 -0500

    Add files via upload
    decodificador

--- src/cipher.js
+++ src/cipher.js
@@ -9,20 +9,37 @@ var inicio = 0;
      var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
      var formula = (x -65+incremento)%26+65; //formula laboratoria
      var letracodificada = String.fromCharCode(formula); //sustitución por la variable introducida
-
+    
      document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
      
      inicio++;
   }
-  
-  var div2 = document.getElementById("div2")
-  
-  div2.style.display = "none"
+  function decipher(string,incremento){
+    var longitud = string.length;
+    var inicio = 0; 
+    while (inicio  < longitud)
+ { 
+    //console.log(inicio);
+    var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
+    var formula = (x -65-incremento)%26+65; //formula laboratoria
+    var letracodificada = String.fromCharCode(formula); //sustitución por la variable introducida
 
-  var div3 = document.getElementById("div3")
-  
-  div3.style.display = "block"
+    document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
+    
+    inicio++;
+ }
+ var div2 = document.getElementById("div2")
+ 
+ div2.style.display = "none"
 
+ var div3 = document.getElementById("div3")
+ 
+ div3.style.display = "block"
+ 
+ var div4 = document.getElementById("div4")
+ 
+ div2.style.display = "block"
 
-
+ 
 }
+  

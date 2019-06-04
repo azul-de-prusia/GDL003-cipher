34a3ce4091a69e55aba7945dc4c9fb52cbfca29e
azul-de-prusia <51216447+azul-de-prusia@users.noreply.github.com>
2019-06-04 15:00:06 -0500

    Add files via upload
    mas diseño

--- src/cipher.js
+++ src/cipher.js
@@ -8,12 +8,13 @@ var inicio = 0;
      //console.log(inicio);
      var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
      var formula = (x -65+incremento)%26+65; //formula laboratoria
-     var letracodificada = String.fromCharCode(formula);
+     var letracodificada = String.fromCharCode(formula); //sustitución por la variable introducida
 
      document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
      
      inicio++;
   }
+  
   var div2 = document.getElementById("div2")
   
   div2.style.display = "none"

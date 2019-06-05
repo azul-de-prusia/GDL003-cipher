31ec2edc05bf2a98f3fe6c877c6283f123502dac
azul-de-prusia <51216447+azul-de-prusia@users.noreply.github.com>
2019-06-05 16:08:50 -0500

    botones y div página de inicio

--- src/cipher.js
+++ src/cipher.js
@@ -1,65 +1,82 @@
 //cipher
 function cipher(string,incremento){
   
-var longitud = string.length;
-var inicio = 0;
-  while (inicio  < longitud)
-  { 
-     //console.log(inicio);
-     var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
-     console.log(x)
-     var formula = (x -65+incremento)%26+65; //formula laboratoria
-     console.log(formula)
-     var letracodificada = String.fromCharCode(formula); //sustitución por la variable introducida
-    
-
-     console.log(letracodificada)
-     document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
-     
-     inicio++;
-  }
-   var div2 = document.getElementById("div2")
-
-   div2.style.display = "none"
-
-   var div3 = document.getElementById("div3")
-
-   div3.style.display = "block"
-   var div4 = document.getElementById("div4")
-
-   div4.style.display = "block"
-}
-//cipher
-function decipher(string,incremento){
-  
    var longitud = string.length;
    var inicio = 0;
      while (inicio  < longitud)
      { 
         //console.log(inicio);
-        var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra 
-        console.log(x) 
-        var formula = (x +65-incremento)%26+65; //formula laboratoria
-        console.log(formula)
+        var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra  
+        var formula = (x -65+incremento)%26+65; //formula laboratoria
         var letracodificada = String.fromCharCode(formula); //sustitución por la variable introducida
-    
-       console.log(letracodificada)
-
+       
+   
         document.getElementById("mensajecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
         
         inicio++;
      }
-
+      var div2 = document.getElementById("div2")
+   
+      div2.style.display = "none"
+   
+      var div3 = document.getElementById("div3")
+   
+      div3.style.display = "block"
+      var div4 = document.getElementById("div4")
+   
+      div4.style.display = "none"
    }
-  
-
-   function NumText(string){//solo letras y numeros
-      var out = '';
-      //Se añaden las letras validas
-      var filtro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';//Caracteres validos
+   //decipher
+   function decipher(string,incremento){
      
-      for (var i=0; i<string.length; i++)
-         if (filtro.indexOf(string.charAt(i)) != -1) 
-          out += string.charAt(i);
-      return out;
-   }
+      var longitud = string.length;
+      var inicio = 0;
+        while (inicio  < longitud)
+        { 
+           //console.log(inicio);
+           var x = string.charCodeAt(inicio); //Obtiene el codigo ascii de la letra 
+
+           formula = (x-65-incremento)%26+65; //formula laboratoria
+           console.log(formula)
+           
+           var letracodificada = String.fromCharCode(formula+26); //sustitución por la variable introducida
+            if (letracodificada==="[")
+               {
+               document.getElementById("mensajedecodificado").innerHTML += "A"
+               }
+            else
+               {
+               document.getElementById("mensajedecodificado").innerHTML += letracodificada
+               }
+            
+ 
+
+           //document.getElementById("mensajedecodificado").innerHTML += letracodificada //imprime la letra codificada a partir del codigo ascii 
+           
+           inicio++;
+        }
+        var div2 = document.getElementById("div2")
+   
+        div2.style.display = "none"
+        
+        var div3 = document.getElementById("div3")
+        div3.style.display = "none"
+        
+        var div4 = document.getElementById("div4")
+        div4.style.display = "none"
+
+        var div5 = document.getElementById("div5")
+        div5.style.display = "block"
+      }
+     
+   
+      function NumText(string){//solo letras y numeros
+         var out = '';
+         //Se añaden las letras validas
+         var filtro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';//Caracteres validos
+        
+         for (var i=0; i<string.length; i++)
+            if (filtro.indexOf(string.charAt(i)) != -1) 
+             out += string.charAt(i);
+         return out;
+      }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log ("mercado negro de arte");
+function muestragrafica() {
+
+    document.getElementById('div6').style.display = 'block';
+    document.getElementById('div7').style.display = 'none';
+    document.getElementById('inicio').style.display = 'none';
+}
+	
+  
+}
+function muestrapintura() {
+
+	var div7 = document.getElementById("div7")
+	var div6 = document.getElementById("div6")
+    var inicio= doc
+    
+	div7.style.display = "none"
+	div6.style.display = "block"
+    inicio.display = "none"
+}
